6d6b4efd80b5f4f371ad6d93e5decdc60c546840
YuliAdam <ylia_adamovich@mail.ru>
2024-10-09 23:38:02 +0200

    feat: add level, play again, money functionality

--- random-game/script.js
+++ random-game/script.js
@@ -13,6 +13,10 @@ let audio = document.querySelector('.treck');
 let gameTime = document.querySelector('.game-time');
 let gameOver = document.querySelector('.game-over');
 let tableRows = document.querySelector('.table-rows');
+let playAgain = document.querySelector('.play-again');
+let level = document.querySelector('.level').children;
+const speedCactus = ['4s', '3s', '2s'];
+let timeMax = 10;
 
 //Init game
 
@@ -22,6 +26,7 @@ function playAudio() {
   }
 
 function playGame() {
+    timeMax = 10;
     if(gamer.value === '') gamer.value = 'Anonim gamer';
     console.log(gamer.value);
     gamerList.push(gamer.value);
@@ -54,8 +59,6 @@ window.addEventListener('keydown',function(e){
 
 //Change game time
 
-let timeMax = 90;
-
 function changeTime(){
     let sec = Math.floor((timeMax - audio.currentTime)%60);
     if(sec<10) sec = '0' + sec;
@@ -69,10 +72,16 @@ function pauseAudio() {
     audio.pause();
 }
 
+    //Show table
+
+
 function showGameOver(){
-    for (let i=0; i<gamerList.length;i++){
+    tableRows.innerHTML='';
+    if(gamerList.length > 10) gamerList.splice(0,1);
+    if(gamerTimeList.length > 10) gamerTimeList.splice(0,1);
+    for (let i=gamerList.length; i > 0; i--){
         const tr = document.createElement("tr");
-        tr.innerHTML = "<td>"+ gamerList[i] +"</td>" + "<td>"+ gamerTimeList[i] +"</td>" ;
+        tr.innerHTML = "<td>"+ gamerList[i-1] +"</td>" + "<td>"+ gamerTimeList[i-1] +"</td>" ;
         tableRows.appendChild(tr);
     }
     gameOver.classList.add('over');
@@ -86,6 +95,11 @@ function stopGame() {
 
 }
 
+function convertCurrentTime(time){
+    let sec = Math.floor(time%60);
+    if(sec<10) sec = '0' + sec;
+    return Math.floor(time/60) + ':'+ sec;
+  } 
 
 let isAlive = setInterval(function(){
     changeTime();
@@ -96,14 +110,75 @@ let isAlive = setInterval(function(){
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
     if(cactusRight > dinoRight && cactusLeft > dinoLeft 
         && dinoBottom <= parseInt(window.getComputedStyle(cactus).getPropertyValue('height'))*0.25){
-        gamerTimeList.push(gameTime.innerHTML);
+        gamerTimeList.push(convertCurrentTime(audio.currentTime));
         pauseAudio();
         stopGame();
     }
+    if(audio.currentTime > timeMax){
+        pauseAudio();
+        gamerTimeList.push(convertCurrentTime(audio.currentTime));
+        audio.currentTime=0;
+        stopGame();
+        cactus.classList.remove('run');
+        money.classList.remove('run-money');
+        money1.classList.remove('run-money1');
+    }
 },10);
 
+//Game again
 
+playAgain.addEventListener('click',function(){
+    gameOver.classList.remove('over');
+    document.querySelector('.play').style.display = "flex";
+    
+})
 
+//Change level
+level[1].classList.add('level-activ');
+for(let i =0; i< level.length;i++){
+    level[i].addEventListener('click',function(){
+        for(let el of level) el.classList.remove('level-activ');
+        cactus.style['animation-duration'] = speedCactus[i];
+        level[i].classList.add('level-activ');
+    })
+}
+
+// Take money
+
+let takeMoney = setInterval(function(){
+    let dinoBottom = parseInt(window.getComputedStyle(dino).getPropertyValue('bottom'));
+    let dinoRight = parseInt(window.getComputedStyle(dino).getPropertyValue('right')) - parseInt(window.getComputedStyle(dino).getPropertyValue('width'))*0.5;
+    let dinoLeft = parseInt(window.getComputedStyle(dino).getPropertyValue('left')) - parseInt(window.getComputedStyle(dino).getPropertyValue('width'))*0.5;
+    let moneyRight = parseInt(window.getComputedStyle(money).getPropertyValue('right'));
+    let moneyLeft = parseInt(window.getComputedStyle(money).getPropertyValue('left'));
+    if(moneyRight > dinoRight && moneyLeft > dinoLeft 
+        && dinoBottom >= parseInt(window.getComputedStyle(money).getPropertyValue('bottom'))*0.25){
+        money.classList.add('take-money');
+        timeMax = timeMax + 5;
+        setTimeout(function(){
+            money.classList.remove('take-money');
+        },1000)    
+    }
+    let money1Right = parseInt(window.getComputedStyle(money1).getPropertyValue('right'));
+    let money1Left = parseInt(window.getComputedStyle(money1).getPropertyValue('left'));
+    if(money1Right > dinoRight && money1Left > dinoLeft 
+        && dinoBottom >= parseInt(window.getComputedStyle(money1).getPropertyValue('bottom'))*0.25){
+        money1.classList.add('take-money');
+        timeMax = timeMax + 5;
+        setTimeout(function(){
+            money1.classList.remove('take-money');
+        },1000)    
+    }
+    if(audio.currentTime > timeMax){
+        pauseAudio();
+        gamerTimeList.push(convertCurrentTime(audio.currentTime));
+        audio.currentTime=0;
+        stopGame();
+        cactus.classList.remove('run');
+        money.classList.remove('run-money');
+        money1.classList.remove('run-money1');
+    }
+},10);
 
 
 
